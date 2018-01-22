f16951fade5d5ca796fd384f2534dcbbefac7594
Eric Seguin <eric@segwin.ca>
2018-01-22 14:39:50 -0500

    Add tests project to solution
    - Add "Tests" project to solution to build and run test cases
    - Add Visual Studio artifacts to .gitignore

---
+++ Source/Test_MainPipeline.cpp
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    Test_MainPipeline.cpp
+
+  ==============================================================================
+*/
+
+#include "catch.hpp"
+
+#include "MainPipeline.h"
+
+/**
+ * How to write tests with Catch:
+ * https://github.com/catchorg/Catch2/blob/2bbba4f5444b7a90fcba92562426c14b11e87b76/docs/tutorial.md#writing-tests
+ */
+
+//TEST_CASE("(TODO) Test case description", "[MainPipeline]") {
+//}
---
+++ Source/Test_TimeStretch.cpp
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    Test_TimeStretch.cpp
+
+  ==============================================================================
+*/
+
+#include "catch.hpp"
+
+#include "TimeStretch.h"
+
+/**
+ * How to write tests with Catch:
+ * https://github.com/catchorg/Catch2/blob/2bbba4f5444b7a90fcba92562426c14b11e87b76/docs/tutorial.md#writing-tests
+ */
+
+//TEST_CASE("(TODO) Test case description", "[TimeStretch]") {
+//}
