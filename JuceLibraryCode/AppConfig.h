2a62b5af670010da3d9b97683c2c08f28e638b09
Eric Seguin <eric@segwin.ca>
2018-01-31 17:12:08 -0500

    Copy modules into source control using Projucer
    * Change reduces external dependencies when compiling project
        * Now: only external dependency user has to install is VST SDK (should this also be included as a submodule?)
    * Other minor tweaks to .jucer file (company name, AAX category)

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -381,7 +381,7 @@
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
 #endif
 #ifndef  JucePlugin_CFBundleIdentifier
- #define JucePlugin_CFBundleIdentifier     com.yourcompany.QuantumReverb
+ #define JucePlugin_CFBundleIdentifier     ca.quantum-music.QuantumReverb
 #endif
 #ifndef  JucePlugin_RTASCategory
  #define JucePlugin_RTASCategory           ePlugInCategory_None
@@ -399,7 +399,7 @@
  #define JucePlugin_RTASDisableMultiMono   0
 #endif
 #ifndef  JucePlugin_AAXIdentifier
- #define JucePlugin_AAXIdentifier          com.yourcompany.QuantumReverb
+ #define JucePlugin_AAXIdentifier          ca.quantum-music.QuantumReverb
 #endif
 #ifndef  JucePlugin_AAXManufacturerCode
  #define JucePlugin_AAXManufacturerCode    JucePlugin_ManufacturerCode
@@ -408,7 +408,7 @@
  #define JucePlugin_AAXProductId           JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_AAXCategory
- #define JucePlugin_AAXCategory            AAX_ePlugInCategory_Dynamics
+ #define JucePlugin_AAXCategory            AAX_ePlugInCategory_Reverb
 #endif
 #ifndef  JucePlugin_AAXDisableBypass
  #define JucePlugin_AAXDisableBypass       0
