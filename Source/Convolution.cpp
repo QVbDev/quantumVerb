158299a8cf878d2c83fd25f86d3e4e99973e7274
Eve <eve.hamilton@hotmail.com>
2018-02-09 00:29:12 -0500

    Complete methods of the Convolution class.

--- Source/Convolution.cpp
+++ Source/Convolution.cpp
@@ -1,9 +1,9 @@
 /*
-  ==============================================================================
+==============================================================================
 
-    Convolution.cpp
+Convolution.cpp
 
-  ==============================================================================
+==============================================================================
 */
 
 #include "Convolution.h"
@@ -11,41 +11,65 @@
 namespace reverb
 {
 
-    //==============================================================================
-    /**
-     * @brief (TODO) Brief description
-     *
-     * (TODO) Detailed description
-     *
-     * @param [in] processor    Pointer to main processor
-     */
-    Convolution::Convolution(juce::AudioProcessor * processor)
-        : Task(processor)
-    {
-    }
+	//==============================================================================
+	/**
+	* @brief Constructor. Creates a Convolution object.
+	*
+	* @details Creates a Convolution object associated to an AudioProcessor.
+	*
+	* @param [in] processor    Pointer to main processor.
+	*/
+	Convolution::Convolution(juce::AudioProcessor* processor)
+		: Task(processor)
+	{
+	}
 
-    //==============================================================================
-    /**
-     * @brief (TODO) Brief description
-     *
-     * (TODO) Detailed description
-     *
-     * @param [in,out] audio    (TODO) Parameter description
-     */
-    void Convolution::exec(juce::AudioSampleBuffer& audio)
-    {
-    }
+	//==============================================================================
+	/**
+	* @brief Main function of the Convolution class. Executes the convolution of
+	*        the audio buffer with the IR.
+	*
+	* @details Creates a ProcessContext object that specifies the audio buffer to
+	*          convolve and the buffer to contain the resulted signal. Calls the
+	*          juce::dsp::Convolution::process() function to compute the
+	*          convolution.
+	*
+	* @param [in,out] audio    The audio buffer to be convolved with the IR.
+	*/
+	void Convolution::exec(juce::AudioSampleBuffer& audio)
+	{
+		juce::dsp::AudioBlock<float> audioBlock(audio);
+		juce::dsp::AudioBlock<float> outBlock(audio);
+		juce::dsp::ProcessContextNonReplacing<float> context(audioBlock, outBlock);
+		process(context);
+	}
 
-    //==============================================================================
-    /**
-    * @brief Brief description
-    *
-    * Detailed description
-    *
-    * @param [in] ir    Parameter description
-    */
-    void Convolution::loadIR(juce::AudioSampleBuffer& ir)
-    {
-    }
-
+	//==============================================================================
+	/**
+	* @brief Defines the main characteristics of the convolution process and loads
+	*        the IR into the juce::dsp::Convolution object.
+	*
+	* @details Calls JUCE Convolution class's functions prepare() and
+	*          copyAndLoadImpulseResponseFromBuffer().
+	*
+	* @param [in] ir    The IR signal to convolve with the audio buffer.
+	*/
+	void Convolution::loadIR(juce::AudioSampleBuffer& ir)
+	{
+		juce::dsp::ProcessSpec spec;
+		spec.sampleRate = 44100; // 44.1 kHz
+		// If the block size is small, the overlap-add method requires a small amount of RAM
+		// at the expense of increasing computational load. If the block size is large, the
+		// overlap-add method is more efficient computationally, at the expense of the memory
+		// required. It is relatively difficult to find an optimal block size , since the
+		// implementation is machine-dependent.
+		// https://books.google.ca/books?id=VQs_Ly4DYDMC&pg=PA130&lpg=PA130&dq=convolution+algorithm+optimal+block+size&source=bl&ots=jImfjKud-t&sig=SjLhgdAnfac0_6He7RBl0O-Snu8&hl=en&sa=X&ved=0ahUKEwj4y9367IDZAhWL24MKHV28AXoQ6AEIOzAD#v=onepage&q=convolution%20algorithm%20optimal%20block%20size&f=false
+		spec.maximumBlockSize = 1024;
+		spec.numChannels = ir.getNumChannels();
+		// Must be called before loading the impulse response to provide to the convolution
+		// the maximumBufferSize to handle and the sample rate for optional resampling.
+		prepare(spec);
+		copyAndLoadImpulseResponseFromBuffer(ir, spec.sampleRate,
+			(ir.getNumChannels() == 1 ? false : true), false, ir.getNumSamples());
+	}
 }
--- Source/Convolution.h
+++ Source/Convolution.h
@@ -1,9 +1,9 @@
 /*
-  ==============================================================================
+==============================================================================
 
-    Convolution.h
+Convolution.h
 
-  ==============================================================================
+==============================================================================
 */
 
 #pragma once
@@ -13,25 +13,26 @@
 namespace reverb
 {
 
-    //==============================================================================
-    /**
-     * TODO: Description
-     */
-    class Convolution : public Task,
-                        public juce::dsp::Convolution
-    {
-    public:
-        //==============================================================================
-        Convolution(juce::AudioProcessor * processor);
+	//==============================================================================
+	/**
+	* @brief Derives from JUCE's Convolution base class. Computes the convolution
+	*        algorithm between the audio signal and the IR buffer.
+	*/
+	class Convolution : public Task,
+		public juce::dsp::Convolution
+	{
+	public:
+		//==============================================================================
+		Convolution(juce::AudioProcessor* processor);
 
-        //==============================================================================
-        using Ptr = std::shared_ptr<Convolution>;
+		//==============================================================================
+		using Ptr = std::shared_ptr<Convolution>;
 
-        //==============================================================================
-        virtual void exec(juce::AudioSampleBuffer& audio) override;
+		//==============================================================================
+		virtual void exec(juce::AudioSampleBuffer& audio) override;
 
-        //==============================================================================
-        void loadIR(juce::AudioSampleBuffer& ir);
-    };
+		//==============================================================================
+		void loadIR(juce::AudioSampleBuffer& ir);
+	};
 
 }
