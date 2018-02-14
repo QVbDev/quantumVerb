1943abc495e5d02596b67b71b5576b26d9eb8e47
Eric Seguin <eric@segwin.ca>
2018-02-14 15:48:36 -0500

    Add PreDelay template files to project
    - Rebuild Visual Studio solution

---
+++ Source/PreDelay.cpp
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+    PreDelay.cpp
+    Created: 14 Feb 2018 3:26:12pm
+    Author:  Eric Seguin
+
+  ==============================================================================
+*/
+
+#include "PreDelay.h"
+
+namespace reverb
+{
+
+    //==============================================================================
+    /**
+     * @brief (TODO) Brief description
+     *
+     * (TODO) Detailed description
+     *
+     * @param [in] processor    Pointer to main processor
+     */
+    PreDelay::PreDelay(juce::AudioProcessor * processor)
+        : Task(processor)
+    {
+    }
+
+    //==============================================================================
+    /**
+     * @brief (TODO) Brief description
+     *
+     * (TODO) Detailed description
+     *
+     * @param [in,out] ir (TODO) Parameter description
+     */
+    void PreDelay::exec(juce::AudioSampleBuffer& ir)
+    {
+    }
+
+}
---
+++ Source/Test_PreDelay.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    Test_PreDelay.cpp
+    Created: 14 Feb 2018 3:27:04pm
+    Author:  Eric Seguin
+
+  ==============================================================================
+*/
+
+#include "catch.hpp"
+
+#include "Mixer.h"
+
+/**
+ * How to write tests with Catch:
+ * https://github.com/catchorg/Catch2/blob/2bbba4f5444b7a90fcba92562426c14b11e87b76/docs/tutorial.md#writing-tests
+ */
+
+//TEST_CASE("(TODO) Test case description", "[PreDelay]") {
+//}
