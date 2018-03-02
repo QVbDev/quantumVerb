11078535336e9243ba60367e73c751b5b497b5a5
Alexandre Maltais <alexandre.maltais-tremblay@polymtl.ca>
2018-03-01 20:37:02 -0500

    Polished code

--- Source/Equalizer.cpp
+++ Source/Equalizer.cpp
@@ -11,19 +11,22 @@ Equalizer.cpp
 namespace reverb {
 
 	Equalizer::Equalizer(juce::AudioProcessor * processor, int numFilters)
-		: Task(processor) {
+		: Task(processor) 
+	{
 
 		if (numFilters < 3) throw WrongFilterNumber();
 
 		filterSet.add(new LowShelfFilter(processor));
 		
-		for (int i = 0; i < numFilters - 2; i++) {
+		for (int i = 0; i < numFilters - 2; i++) 
+		{
 			filterSet.add(new PeakFilter(processor));
 		}
 
 		filterSet.add(new HighShelfFilter(processor));
 
-		for (int i = 0; i < numFilters; i++) {
+		for (int i = 0; i < numFilters; i++) 
+		{
 			EQGains.push_back(1.0f);
 		}
 
@@ -32,7 +35,8 @@ namespace reverb {
 		filterSet[0]->setQ(0.71);
 		filterSet[numFilters - 1]->setQ(0.71);
 
-		for (int i = 0; i < numFilters; i++) {
+		for (int i = 0; i < numFilters; i++)
+		{
 			EQGains[i] = 2;
 			filterSet[i]->setGain(2);
 			filterSet[i]->setFrequency((i + 1) * 10000 / numFilters);
@@ -48,7 +52,8 @@ namespace reverb {
 	void Equalizer::exec(juce::AudioSampleBuffer& ir) {
 		updateFilters();
 
-		for (int i = 0; i < filterSet.size(); i++) {
+		for (int i = 0; i < filterSet.size(); i++) 
+		{
 			filterSet[i]->exec(ir);
 		}
 	}
@@ -65,7 +70,8 @@ namespace reverb {
 
 		evalFrequencies[0] = 0;
 
-		for (int i = 1; i < dim - 1; i++) {
+		for (int i = 1; i < dim - 1; i++) 
+		{
 			evalFrequencies[i] = filterSet[i]->frequency;
 		}
 
@@ -83,12 +89,14 @@ namespace reverb {
 
 		//Compute gamma column
 
-		for (int i = 0; i < dim; i++) {
+		for (int i = 0; i < dim; i++) 
+		{
 			gamma_data[i] = filterSet[i]->getdBAmplitude(evalFrequencies[i]);
 		}
 
 		//Set gains to 1 dB for B matrix
-		for (int i = 0; i < dim; i++) {
+		for (int i = 0; i < dim; i++) 
+		{
 			filterSet[i]->setGain(Filter::invdB(1.0f));
 		}
 		
@@ -96,14 +104,17 @@ namespace reverb {
 
 		//Correction algorithm (5 iterations)
 
-		for (int k = 0; k < 5; k++) {
+		for (int k = 0; k < 5; k++) 
+		{
 
 			memcpy(lambda_data, gamma_data, dim * sizeof(float));
 
 			//Compute B matrix with new gains
-			for (int i = 0; i < dim; i++) {
+			for (int i = 0; i < dim; i++) 
+			{
 
-				for (int j = 0; j < dim; j++) {
+				for (int j = 0; j < dim; j++) 
+				{
 
 					B_data[j + B.getNumColumns() * i] = filterSet[j]->getdBAmplitude(evalFrequencies[i]);
 				}
@@ -111,7 +122,8 @@ namespace reverb {
 
 			B.solve(lambda);
 
-			for (int i = 0; i < dim; i++) {
+			for (int i = 0; i < dim; i++) 
+			{
 
 				filterSet[i]->setGain(Filter::invdB(lambda_data[i] * Filter::todB(filterSet[i]->gainFactor)));
 			}
@@ -128,10 +140,12 @@ namespace reverb {
 	* @param [in] freq   Frequency at which the filter magnitude is evaluated
 	*/
 
-	float Equalizer::getdBAmplitude(float freq) {
+	float Equalizer::getdBAmplitude(float freq) 
+	{
 		float dBAmplitude = 0;
 
-		for (int i = 0; i < filterSet.size(); i++) {
+		for (int i = 0; i < filterSet.size(); i++) 
+		{
 			dBAmplitude += filterSet[i]->getdBAmplitude(freq);
 		}
 
@@ -139,75 +153,76 @@ namespace reverb {
 
 	}
 
-	void Equalizer::setFilterFrequency(float freq, int num) {
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+	void Equalizer::setFilterFrequency(float freq, int num) 
+	{
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 		//Check whether filter frequency crosses another one's
 
-		if (num == 0) {
-			if (freq >= filterSet[num + 1]->frequency)
-				throw WrongEQFrequency();
+		if (num == 0) 
+		{
+			if (freq >= filterSet[num + 1]->frequency) throw WrongEQFrequency();
 		}
 
-		if (num == (filterSet.size() - 1)) {
-			if (freq <= filterSet[num - 1]->frequency)
-				throw WrongEQFrequency();
+		if (num == (filterSet.size() - 1))
+		{
+			if (freq <= filterSet[num - 1]->frequency) throw WrongEQFrequency();
 		}
 
-		else {
-			if (freq <= filterSet[num - 1]->frequency || freq >= filterSet[num + 1]->frequency)
-				throw WrongEQFrequency();
+		else 
+		{
+			if (freq <= filterSet[num - 1]->frequency || freq >= filterSet[num + 1]->frequency) throw WrongEQFrequency();
 		}
 
 			filterSet[num]->setFrequency(freq);
 	}
-	void Equalizer::setFilterGain(float gain, int num) {
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+	void Equalizer::setFilterGain(float gain, int num) 
+	{
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 			EQGains[num] = gain;
 
 			filterSet[num]->setGain(gain);
 
 	}
-	void Equalizer::setFilterQ(float Q, int num) {
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+	void Equalizer::setFilterQ(float Q, int num) 
+	{
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 			filterSet[num]->setQ(Q);
 	}
 
-	float Equalizer::getFilterFrequency(int num) {
+	float Equalizer::getFilterFrequency(int num) 
+	{
 
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 		return filterSet[num]->frequency;
 	}
 
-	float Equalizer::getFilterGain(int num) {
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+	float Equalizer::getFilterGain(int num) 
+	{
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 		return filterSet[num]->gainFactor;
 	}
 
-	float Equalizer::getFilterQ(int num) {
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+	float Equalizer::getFilterQ(int num) 
+	{
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 		return filterSet[num]->gainFactor;
 	}
 
-	float Equalizer::getEQGain(int num) {
-		if (num < 0 || num >= filterSet.size())
-			throw InvalidFilterException();
+	float Equalizer::getEQGain(int num) 
+	{
+		if (num < 0 || num >= filterSet.size()) throw InvalidFilterException();
 
 		return EQGains[num];
 	}
 
-	int Equalizer::getNumFilters() {
+	int Equalizer::getNumFilters() 
+	{
 		return filterSet.size();
 	}
 }
